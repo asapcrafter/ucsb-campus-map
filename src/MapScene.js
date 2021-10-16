c2dab0fea33619b5415456ed3b9906d456d2b915
Steve <asapcrafter@gmail.com>
2021-10-16 16:34:57 -0700

    added mapbox div, correctly renders now

--- src/MapScene.js
+++ src/MapScene.js
@@ -5,19 +5,15 @@ const MapScene = () => {
     // TO MAKE THE MAP APPEAR YOU MUST
     // ADD YOUR ACCESS TOKEN FROM
     // https://account.mapbox.com
-    mapboxgl.accessToken = '<your access token here>';
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhcGNyYWZ0ZXIiLCJhIjoiY2t1cnJyNXVpMGt3ZDJub2RhYWUwMXFmOCJ9.uGtKs3SkPq5kvpSC8xXMdQ';
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
+        center: [-119.847, 34.413 ], // starting position [lng, lat]
+        zoom: 15 // starting zoom
     });
 
-    return (
-        <div>
-            
-        </div>
-    );
+    return null;
 }
 
 export default MapScene;
