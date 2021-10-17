f35976f8708354a6c8988444a1b017f27cf95269
Steve <asapcrafter@gmail.com>
2021-10-16 21:04:43 -0700

    changed mapbox into react component

--- src/MapScene.js
+++ src/MapScene.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; 
+import './styles/css/map.css'
 
 const MapScene = () => {
-    // TO MAKE THE MAP APPEAR YOU MUST
-    // ADD YOUR ACCESS TOKEN FROM
-    // https://account.mapbox.com
     mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhcGNyYWZ0ZXIiLCJhIjoiY2t1cnJyNXVpMGt3ZDJub2RhYWUwMXFmOCJ9.uGtKs3SkPq5kvpSC8xXMdQ';
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/asapcrafter/ckuuf5ndeqvr017pjaxtpgsra', // style URL
         center: [-119.847, 34.413 ], // starting position [lng, lat]
         zoom: 15 // starting zoom
     });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MapScene from './MapScene'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <MapScene />
   </React.StrictMode>,
   document.getElementById('root')
 );
