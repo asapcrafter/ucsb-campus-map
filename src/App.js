12093010682c568b21f6dff9449b0a05a4176ff6
Steve <asapcrafter@gmail.com>
2021-10-16 22:07:46 -0700

    removed bottom logo and redid map component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { useRef, useEffect} from 'react';
-import mapboxgl from 'mapbox-gl';
-import './styles/css/map.css'
+import React from 'react';
+import './styles/css/map.css';
+import MapScene from './MapScene'
 
 function App() {
-	// MapBox react component
-	const mapContainer = useRef(null);
-	const map = useRef(null);
-	
-	mapboxgl.accessToken ='pk.eyJ1IjoiYXNhcGNyYWZ0ZXIiLCJhIjoiY2t1cnJyNXVpMGt3ZDJub2RhYWUwMXFmOCJ9.uGtKs3SkPq5kvpSC8xXMdQ'
-
-	useEffect(() => {
-		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: 'mapbox://styles/asapcrafter/ckuuf5ndeqvr017pjaxtpgsra', // style URL
-			center: [-119.847, 34.413 ], // starting position [lng, lat]
-			zoom: 15 // starting zoom
-		});
-	});
 
 	return (
 		<div className="App">
-			<div ref={mapContainer} className="map-container" />
+			<MapScene />
 		</div>
 	);
 }
--- src/MapScene.js
+++ src/MapScene.js
@@ -3,15 +3,42 @@ import mapboxgl from 'mapbox-gl';
 import './styles/css/map.css'
 
 const MapScene = () => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhcGNyYWZ0ZXIiLCJhIjoiY2t1cnJyNXVpMGt3ZDJub2RhYWUwMXFmOCJ9.uGtKs3SkPq5kvpSC8xXMdQ';
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/asapcrafter/ckuuf5ndeqvr017pjaxtpgsra', // style URL
-        center: [-119.847, 34.413 ], // starting position [lng, lat]
-        zoom: 15 // starting zoom
+    // MapBox react component
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-119.8499);
+    const [lat, setLat] = useState(34.4118);
+    const [zoom, setZoom] = useState(15.5);
+
+    mapboxgl.accessToken ='pk.eyJ1IjoiYXNhcGNyYWZ0ZXIiLCJhIjoiY2t1cnJyNXVpMGt3ZDJub2RhYWUwMXFmOCJ9.uGtKs3SkPq5kvpSC8xXMdQ'
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/asapcrafter/ckuuf5ndeqvr017pjaxtpgsra', // style URL
+            center: [lng, lat],
+            zoom: zoom
+        });
     });
 
-    return null;
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+        setLng(map.current.getCenter().lng.toFixed(4));
+        setLat(map.current.getCenter().lat.toFixed(4));
+        setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
+    return (
+        <div id='mapbox'>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
 }
 
 export default MapScene;
